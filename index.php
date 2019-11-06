4c6eac719bc12220d3cdc6f589c493d238770242
J-a-r-e-k <23661244+J-a-r-e-k@users.noreply.github.com>
2019-11-06 23:18:23 +0100

    Changing translations file names

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 	require_once 'i18n.class.php';
 	$_GET['lang'] = in_array($_GET['lang'], ['pl', 'en', 'de']) ? $_GET['lang'] : 'pl';
-	$i18n = new i18n('lang/lang_{LANGUAGE}.ini', 'cache/', 'pl');
+	$i18n = new i18n('lang/{LANGUAGE}.ini', 'cache/', 'pl');
 	$i18n->init();
 ?>
 
@@ -12,4 +12,4 @@
 </p>
 
 <p>A greeting: <?= L::greeting; ?></p>
-<p>Something other: <?=L::somethingother; ?></p>
+<p>Something other: <?= L::somethingother; ?></p>
