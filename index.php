59e998e6a46e62848f2db3b7f0db9b848528a442
J-a-r-e-k <23661244+J-a-r-e-k@users.noreply.github.com>
2019-11-06 19:07:48 +0100

    Prepare index.php for 3 languages pl, en and de

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+	require_once 'i18n.class.php';
+	$_GET['lang'] = in_array($_GET['lang'], ['pl', 'en', 'de']) ? $_GET['lang'] : 'pl';
+	$i18n = new i18n('lang/lang_{LANGUAGE}.ini', 'cache/', 'pl');
+	$i18n->init();
+?>
+
+<p>
+	<a href="/">PL</a>
+	<a href="/en">EN</a>
+	<a href="/de">DE</a>
+</p>
+
+<p>A greeting: <?= L::greeting; ?></p>
+<p>Something other: <?=L::somethingother; ?></p>
